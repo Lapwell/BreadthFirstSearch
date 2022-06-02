ae731bf592b5e8d5c35e5a39c6c1738e44c36c47
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-06-01 22:24:16 -0700

    Completed:     -Set up window.     -Create grid.     -Mouse interaction.
    To Do:
        -Spawn walls at mouse click pos.
        -Move start/goal with mouse.
        -Path finding.
        -Render path finding.
        -Display values of path.
    
    Optional:
        -Colour density option for path value display.

--- main.py
+++ main.py
@@ -3,27 +3,47 @@ import sys
 
 pygame.init()
 
-BLACK = 0, 0, 0
 WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+GREY = 64, 64, 64
 RED = 255, 0, 0
 GREEN = 0, 255, 0
 BLUE = 0, 0, 255
 
-WIDTH, HEIGHT = 800, 800
-ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
+OFFSET = 2  # Offset is the thickness of the black lines.
+SQR_SIZE = 20  # The size of the squares in the grid.
+# print("Enter 'n' for default value.")
+# X_SQUARES, Y_SQUARES = input('Input X amount:'), input("Input Y amount:")  # The amount of squares in the x-axis and y-axis.
+X_SQUARES, Y_SQUARES = 'n', 'n'
+if X_SQUARES or Y_SQUARES == 'n':
+    X_SQUARES, Y_SQUARES = 40, 40
+
+
+WIDTH, HEIGHT = (X_SQUARES * OFFSET) + (SQR_SIZE * X_SQUARES) + OFFSET, (Y_SQUARES * OFFSET) + (SQR_SIZE * Y_SQUARES) + OFFSET
+ROOT = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 FPS = 60
 clock = pygame.time.Clock()
 FONT = pygame.font.Font(None, 24)
 
-wsize, hsize = WIDTH // 20, HEIGHT // 20
-row_size, column_size = WIDTH // wsize, HEIGHT // hsize
-grid_square = pygame.Rect(0, 0, wsize, hsize)
-grid = []
+grid = []  # This list is for the grid of squares to be stored in.
+
+
+class GridSquare:
+    def __init__(self, posx, posy, colour):
+        self.colour = colour
+        self.rect = pygame.Rect(posx, posy, SQR_SIZE, SQR_SIZE)
+
+
+def mouse_click():
+    for item in grid:
+        if item.rect.collidepoint(pygame.mouse.get_pos()):
+            item.colour = GREY
+            print('Collide true', pygame.mouse.get_pos())
 
 
 def fps_counter():
     count = str(int(clock.get_fps()))
-    fps_txt = FONT.render(count, True, WHITE)
+    fps_txt = FONT.render(count, True, GREEN)
     ROOT.blit(fps_txt, (fps_txt.get_width() - fps_txt.get_width()//2, fps_txt.get_height() - fps_txt.get_height()//2))
 
 
@@ -33,10 +53,14 @@ def check_events():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP and pygame.mouse.get_focused() and event.button == 1:
+            mouse_click()
 
 
 def update_root():
     ROOT.fill(BLACK)
+    for item in grid:
+        pygame.draw.rect(ROOT, item.colour, item.rect)
     fps_counter()
     pygame.display.update()
 
@@ -47,6 +71,13 @@ def main():
     clock.tick(FPS)
 
 
+grid_posx, grid_posy = OFFSET, OFFSET
 if __name__ == "__main__":
+    for i in range(0, Y_SQUARES):
+        for y in range(0, X_SQUARES):
+            grid.append(GridSquare(grid_posx, grid_posy, WHITE))
+            grid_posx += SQR_SIZE + OFFSET
+        grid_posy += SQR_SIZE + OFFSET
+        grid_posx = OFFSET
     while True:
         main()
